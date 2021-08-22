304a6e6bd661d125475b729b59ea12dbf313185b
Katherine Kumar <Katherine2bk@gmail.com>
2021-08-22 14:14:01 -0700

    adding files to branch, merging small project

---
+++ band_generator/band.py
@@ -0,0 +1,5 @@
+# Create a greeting
+print("Welcome to the Band Name Generator")
+cityName = input("What is the name of the city you grew up?\n")
+petName = input("Give the name of a pet\n")
+print("The name of your nand is" + cityName + petName)
